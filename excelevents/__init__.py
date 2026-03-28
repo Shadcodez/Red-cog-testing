4455e2b39bec10e8b97518232cde1d03c1973738
Shadcodez <133531004+Shadcodez@users.noreply.github.com>
2026-03-28 19:30:27 -0400

    Refactor import and setup function for ExcelEvents

--- excelevents/__init__.py
+++ excelevents/__init__.py
@@ -1,4 +1,8 @@
-from .excelevents import ExcelEvents
+# ExcelEvents/__init__.py
+from .core import ExcelEvents
+
+async def setup(bot):
+    await bot.add_cog(ExcelEvents(bot))from .excelevents import ExcelEvents
 
 async def setup(bot):
     await bot.add_cog(ExcelEvents(bot))
