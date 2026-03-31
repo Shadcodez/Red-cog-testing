2519ba93d23c870ff97b647d1beecac1e0495c1a
Shadcodez <133531004+Shadcodez@users.noreply.github.com>
2026-03-30 23:41:11 -0400

    Update __init__.py

--- excelembed/__init__.py
+++ excelembed/__init__.py
@@ -1,5 +1,5 @@
 from redbot.core import commands
-from .excelembeds import Excelembed
+from .excelembeds import excelembed
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(excelembed(bot))
